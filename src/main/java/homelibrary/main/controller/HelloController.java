450986eace65be85c24d09ff457aa707acc676f3
JarnotMaciej <mj300741@student.polsl.pl>
2024-03-13 14:15:13 +0100

    thymeleaf templater

--- src/main/java/homelibrary/main/controller/HelloController.java
+++ src/main/java/homelibrary/main/controller/HelloController.java
@@ -1,13 +1,17 @@
 package homelibrary.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello, World!";
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
